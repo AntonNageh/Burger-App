9fd80f2e57aa4d86af5561583c924c953002b429
Anton Nageh <anton.nageh20@gmail.com>
2025-06-01 14:18:26 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,15 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react';
+import Welcome from './pages/Welcome';
 
 function App() {
+    const User = useUser()
+  if(User.isSignedIn)
+   return ( 
+     <Welcome/>
+   )
   return (
     <header>
       <SignedOut>
@@ -15,6 +22,7 @@ function App() {
         <UserButton />
       </SignedIn>
     </header>
+    
   );
 }
 
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+
+import {
+  ClerkProvider
+} from "@clerk/clerk-react";
+
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import React from "react";
+
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_FRONTEND_API_KEY;
+
+
+
+if (!import.meta.env.VITE_REACT_APP_CLERK_FRONTEND_API_KEY) {
+  throw "Missing Publishable Key"
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <ClerkProvider publishableKey={clerkPubKey} afterSignOutUrl="./">
+        <App />
+    </ClerkProvider>
+  </React.StrictMode>
+)
+
